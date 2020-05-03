2dca1f732b63087f3f1e87de97ec6cf9bcf960d9
sarahj930 <36340586+sarahj930@users.noreply.github.com>
2020-05-03 12:06:04 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -12,6 +12,9 @@ $(document).ready(() => {
     document.getElementById("tapAnywhere").remove();
     document.getElementById("cell1").onclick = function () { clicked1() };
     document.getElementById("cell2").onclick = function () { clicked2() };
+    
+    document.getElementById("cell1").onmouseenter = function () { playMouseSound() };
+    document.getElementById("cell2").onmouseenter = function () { playMouseSound() };
 
     window.speechSynthesis.speak(new SpeechSynthesisUtterance("Hello! Welcome to our game! There are two modes you can play: Tap the left half of the screen to play -- Count the Beats --! Tap the right side of the screen to play -- Making Comparisons!"))
   }
